3a4ea6cd534a4bd48156ea37585eff51baf36b6f
HegoiUrrutia <hegoitolosalh@gmail.com>
2026-02-03 13:01:18 +0100

    Update route.ts

--- src/app/api/genkit/route.ts
+++ src/app/api/genkit/route.ts
@@ -1,4 +1,7 @@
-import {genkit} from '@/ai/genkit';
-import {nextJSHandler} from '@genkit-ai/next';
+import { genkit } from '@/ai/genkit';
+import { nextJSHandler } from '@genkit-ai/next';
+
+// Forzar el uso de Node.js en lugar de Edge
+export const runtime = 'nodejs';
 
 export const POST = nextJSHandler(genkit);
